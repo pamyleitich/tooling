faef2b3891ba21acc5f8752063a7c2252f910d91
pamylelitich <pamyleitich@gmail.com>
2024-10-15 16:49:24 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "bucket" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "pamys3bucket"
+}
+
+variable "table" {
+  description = "The name of the DynamoDB table"
+  type        = string
+  default     = "pamydbtable"
+}
+
+variable "region" {
+  description = "The AWS region"
+  type        = string
+  default     = "us-west-2"
+}
